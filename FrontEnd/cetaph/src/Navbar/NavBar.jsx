87cb041a1ac073cb7f36f9c22745f4e10e2b2c39
WIN-88537DQ39BI\Administrator <gambaropablo4@gmail.com>
2022-09-16 09:23:26 -0300

    Navbar changed

--- FrontEnd/cetaph/src/Navbar/NavBar.jsx
+++ FrontEnd/cetaph/src/Navbar/NavBar.jsx
@@ -11,13 +11,14 @@ const NavBar = () => {
           <li>
             <a href="sass.html">
               <i class="bi bi-heart"></i>
+
             </a>
           </li>
           <li>
             <a href="badges.html"><i class="bi bi-person-circle"></i></a>
           </li>
           <li>
-            <a href="collapsible.html">JavaScript</a>
+            <a href="collapsible.html"><i class="bi bi-basket3"></i></a>
           </li>
         </ul>
       </div>
